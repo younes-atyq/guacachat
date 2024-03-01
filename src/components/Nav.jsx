1526de99fa671b564212c96eca03485bb82d831c
Avocado <yauness190@gmail.com>
2024-03-01 12:53:44 +0100

    add a basic search function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Nav = ({ pageName }) => {
   useEffect(() => {
     document.querySelector(`[data-${pageName}]`)?.classList.add("active");
-  });
+  }, [pageName]);
   return (
     <nav id="nav">
       <ul>
