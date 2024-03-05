f5be12627de522281dfb34b4c4eef9436bd7f1e8
Avocado <yauness190@gmail.com>
2024-03-05 08:08:47 +0100

    deployment error in vercel #test3

--- src/helpers/SendMsgUI.js
+++ src/helpers/SendMsgUI.js
@@ -9,36 +9,40 @@ const sendMsgUI = ({
   currentRoom,
   isAdmin,
 }) => {
-  // const html = convertToHTML(editorState.getCurrentContent());
-  // const message = document.createElement("div");
-  // const textMsg = document.createElement("p");
-  // textMsg.innerHTML = html;
-  // textMsg.className = "text-msg";
-  // const name = document.createElement("h3");
-  // name.className = "name";
-  // // if (isAdmin) {
-  // //   name.id = "admin";
-  // //   name.prepend("@");
-  // // }
-  // // get the username
-  // const username = auth.currentUser.displayName;
-  // // get the current time
-  // let currentMinute = new Date().getMinutes();
-  // let currentHour = new Date().getHours();
-  // // format the time
-  // currentHour = currentHour < 10 ? "0" + currentHour : currentHour;
-  // currentMinute = currentMinute < 10 ? "0" + currentMinute : currentMinute;
-  // const time = currentHour + ":" + currentMinute;
-  // name.innerHTML = `${username} <span class='time'>${time}</span>`;
-  // // append the name and the message
-  // message.append(name);
-  // message.append(textMsg);
-  // // set the converted content
-  // const messageContent = message.innerHTML;
-  // // reset the editor
-  // setEditorState(EditorState.createEmpty());
-  // // send the message
-  // SendMsg({ message: messageContent, username, currentRoom });
+  const html = convertToHTML(editorState.getCurrentContent());
+
+  const message = document.createElement("div");
+  const textMsg = document.createElement("div");
+
+  textMsg.innerHTML = html;
+  textMsg.className = "text-msg";
+
+  const name = document.createElement("h3");
+  name.className = "name";
+  if (isAdmin) {
+    name.id = "admin";
+    name.prepend("@");
+  }
+  // get the username
+  const username = auth.currentUser.displayName;
+  // get the current time
+  let currentMinute = new Date().getMinutes();
+  let currentHour = new Date().getHours();
+  // format the time
+  currentHour = currentHour < 10 ? "0" + currentHour : currentHour;
+  currentMinute = currentMinute < 10 ? "0" + currentMinute : currentMinute;
+  const time = currentHour + ":" + currentMinute;
+
+  name.innerHTML = `${username} <span class='time'>${time}</span>`;
+  // append the name and the message
+  message.append(name);
+  message.append(textMsg);
+  // set the converted content
+  const messageContent = message.innerHTML;
+  // reset the editor
+  setEditorState(EditorState.createEmpty());
+  // send the message
+  SendMsg({ message: messageContent, username, currentRoom });
 };
 
 export default sendMsgUI;
