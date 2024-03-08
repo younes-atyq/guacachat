cac5141087a8ed007e188a318b21658b62ba0e8e
Avocado <yauness190@gmail.com>
2024-03-08 09:22:52 +0100

    rename setUserAllStates

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -15,9 +15,9 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { RoomContext } from "../App";
-import setUserOnline from "../helpers/SetUserOnline";
+import setUserOnline from "../helpers/setUserOnline";
 import { Link, useNavigate } from "react-router-dom";
-import setUserStates from "../helpers/SetUserStates";
+import setUserStates from "../helpers/setUserStates";
 import Popup from "../components/Popup";
 import GetMessages from "../components/GetMessages";
 
